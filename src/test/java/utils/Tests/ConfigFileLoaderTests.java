8a9c8710e08a41fc67f7b23b96444a85c217b9ea
Richard Dickinson <richard.dickinson@digital.justice.gov.uk>
2023-02-02 12:57:38 +0000

    Adding config tests

---
+++ src/test/java/utils/Tests/ConfigFileLoaderTests.java
@@ -0,0 +1,35 @@
+package utils.Tests;
+
+import config.WebDriverConfig;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
+import utils.ConfigLoader;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.CoreMatchers.is;
+
+public class ConfigFileLoaderTests {
+
+    private ConfigLoader configLoader;
+
+    @BeforeTest
+    public void setUp() { this.configLoader = new ConfigLoader(); }
+
+    @Test
+    public void testCanLoadConfigFileWithConfigPropertySetToDefaultAndNoPropertyArgsSet() {
+        System.setProperty("config", "DEFAULT");
+        WebDriverConfig webDriverConfig = configLoader
+                .chooseTargetConfiguration()
+                .build();
+        assertThat(webDriverConfig.getBaseUrl(), is("https://ndelius.test.probation.service.justice.gov.uk"));
+    }
+
+    @Test
+    public void testCanLoadConfigFileWithConfigPropertySetToExternalFile() {
+        System.setProperty("config", "fixtures/test-config.json");
+        WebDriverConfig webDriverConfig = configLoader
+                .chooseTargetConfiguration()
+                .build();
+        assertThat(webDriverConfig.getBaseUrl(), is("https://google.com"));
+    }
+}
