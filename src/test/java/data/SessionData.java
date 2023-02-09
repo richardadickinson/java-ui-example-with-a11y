f99b3ddb1394b3429dc45771ef8e0eb3155a79fb
aurinelle <aurinelle.bouekassa-taboudi@digital.justice.gov.uk>
2023-02-09 11:23:56 +0000

    POJO for SessionData - unfinished

--- src/test/java/data/SessionData.java
+++ src/test/java/data/SessionData.java
@@ -2,22 +2,14 @@ package data;
 
 public class SessionData {
 
-    private static SessionData sessionData = new SessionData();
-    private ThreadLocal<SessionData> threadLocal = new ThreadLocal<>();
-
-    private void setSessionData(){
-        threadLocal.set(sessionData);
-    }
-
-
-    private String Crn;
+    private String crn;
 
     public String getCrn() {
-        return Crn;
+        return crn;
     }
 
     public void setCrn(String crn) {
-        this.Crn = crn;
+        this.crn = crn; //ToDo: Need to think this
     }
 
 
