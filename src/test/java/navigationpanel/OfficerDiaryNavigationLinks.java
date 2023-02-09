44cdf121ff6ea8b1fb60ef1af11e135fd292ec0d
aurinelle <aurinelle.bouekassa-taboudi@digital.justice.gov.uk>
2023-02-09 11:08:00 +0000

    Interfaces for navigation links

---
+++ src/test/java/navigationpanel/OfficerDiaryNavigationLinks.java
@@ -0,0 +1,26 @@
+package navigationpanel;
+
+import pages.officerdiary.AlertsDiaryPage;
+import pages.officerdiary.CaseloadDiaryPage;
+
+import static pages.BasePageObject.clickOnLinkViaText;
+import static utils.webdriver.WebDriverUtils.getWebDriver;
+
+/**
+ * This interface contains a list of method returning the appropriate pageObject by clicking on navigation links that become visible
+ * under the "Officer Diary" section of the main navigation panel.
+ * Classes that implement this interface should ONLY be pageObjects from which these links are visible. e.g. "Caseload Diary" Page
+ */
+public interface OfficerDiaryNavigationLinks {
+
+    default CaseloadDiaryPage clickOnCaseloadDiaryLink() {
+        clickOnLinkViaText("Caseload");
+        return new CaseloadDiaryPage(getWebDriver());
+    }
+
+    default AlertsDiaryPage clickOnAlertsDiaryLink() {
+        clickOnLinkViaText("Alerts Diary");
+        return new AlertsDiaryPage(getWebDriver());
+    }
+
+}
---
+++ src/test/java/navigationpanel/casemanagementlinks/DrugsNavigationLinks.java
@@ -0,0 +1,33 @@
+package navigationpanel.casemanagementlinks;
+
+import pages.casemanagement.drugs.DrugTestListPage;
+import pages.casemanagement.drugs.DrugTestSummaryPage;
+import pages.casemanagement.drugs.DrugsHistoryPage;
+
+import static pages.BasePageObject.clickOnLinkViaText;
+import static utils.webdriver.WebDriverUtils.getWebDriver;
+
+/**
+ * This interface contains a list of method returning the appropriate pageObject by clicking on navigation links that become visible
+ * under the "Drugs" subsection of the "Case Management" section.
+ * Classes that implement this interface should ONLY be pageObjects from which these links are visible. e.g. "Drug Test Summary" Page
+ */
+public interface DrugsNavigationLinks {
+
+    default DrugTestSummaryPage clickOnSummaryViewLink() {
+        clickOnLinkViaText("Summary View");
+        return new DrugTestSummaryPage(getWebDriver());
+    }
+
+    default DrugsHistoryPage clickOnHistoryViewLink() {
+        clickOnLinkViaText("History View");
+        return new DrugsHistoryPage(getWebDriver());
+    }
+
+    default DrugTestListPage clickOnTestListLink() {
+        clickOnLinkViaText("Test List");
+        return new DrugTestListPage(getWebDriver());
+    }
+
+
+}
