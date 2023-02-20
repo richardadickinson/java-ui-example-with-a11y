953be4b3be2f2d1710fe7053d5d25d979e514386
aurinelle <aurinelle.bouekassa-taboudi@digital.justice.gov.uk>
2023-02-20 14:13:28 +0000

    dateUtil method to convert api date

--- src/test/java/utils/DateUtil.java
+++ src/test/java/utils/DateUtil.java
@@ -1,13 +1,23 @@
 package utils;
 
+import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
+import java.util.Arrays;
 
 public class DateUtil {
 
     public static String today() {
         DateTimeFormatter dft = DateTimeFormatter.ofPattern("dd/MM/yyyy");
         return LocalDateTime.now().format(dft);
+    }
+
+    public static String convertApiDate(String apiDate)  {
+        String filtered = Arrays.stream(apiDate.split("T")).toList().get(0);
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd");
+        LocalDate date = LocalDate.parse(filtered, formatter);
+        DateTimeFormatter newFormatter = DateTimeFormatter.ofPattern("dd/MM/yyyy");
+        return date.format(newFormatter);
     }
 
 }
