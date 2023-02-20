f0334141ed0d2b0aab7197a6222e39a5f693a690
aurinelle <aurinelle.bouekassa-taboudi@digital.justice.gov.uk>
2023-02-20 14:30:22 +0000

    requireNonNull

--- src/test/java/data/SessionDataMapper.java
+++ src/test/java/data/SessionDataMapper.java
@@ -3,6 +3,7 @@ package data;
 import testDataApi.Offender;
 
 import java.util.Map;
+import java.util.Objects;
 
 import static stepDefinitions.BaseSteps.*;
 import static testDataApi.Contact.insertContact;
@@ -16,19 +17,19 @@ public class SessionDataMapper {
     public static void createPerson(String path) {
         Map<String, Object> body = Offender.insertOffender(path);
         Person person = new Person().build(body);
-        getSessionData().setPerson(person);
+        getSessionData().setPerson(Objects.requireNonNull(person));
     }
 
     public static void createEvent(String path) {
         Map<String, Object> body  = insertEvent(path, getSessionData().getPerson().getCrn());
         Event event = new Event().build(body);
-        getSessionData().setEvent(event);
+        getSessionData().setEvent(Objects.requireNonNull(event));
     }
 
     public static void createContact(String path){
         Map<String, Object> body = insertContact(path, getSessionData().getPerson().getCrn());
         Contact contact = new Contact().build(body);
-        getSessionData().setContact(contact);
+        getSessionData().setContact(Objects.requireNonNull(contact));
     }
 
 }
