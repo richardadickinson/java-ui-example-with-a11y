ccfb028bccb7dea63aa0e436ff53e08b8ee5f1b8
Richard Dickinson <richard.dickinson@digital.justice.gov.uk>
2023-02-22 16:23:51 +0000

    Add comments on using SearchResultsTable.java

--- src/test/java/utils/webDriver/interactions/SearchResultsTable.java
+++ src/test/java/utils/webDriver/interactions/SearchResultsTable.java
@@ -10,11 +10,22 @@ import java.util.List;
 
 import static utils.webDriver.Builder.getWebDriver;
 
+/**
+ * Methods for working with Search Results tables that match the layout of the National Search results table.
+ * The number of columns and what they contain can change as long as the table conforms to the layout used by the xpaths
+ * to locate the webelements in the table.
+ */
 public class SearchResultsTable {
     private final static Logger logger = LoggerFactory.getLogger(SearchResultsTable.class);
 
+    /**
+     * Selects the View link for the results row that corresponds to the match type and value supplied
+     * tableId - the HTML id for the results table being used
+     * matchType - the data type used to select the correct row e.g. 'CRN' or 'ContactId'
+     * value - the value to be matched against e.g. 'X1234567'
+     */
     public static void selectViewLinkFromSearchResults(String tableId, String matchType, String value) {
-        String matchColumnIndex = getMatchColumnIndex(matchType);
+        String matchColumnIndex = getMatchColumnIndex(tableId, matchType);
         String viewLinkColumnIndex = getViewColumnIndex(tableId);
 
         if (!matchColumnIndex.equals("-1") && !viewLinkColumnIndex.equals("-1")) {
@@ -35,15 +46,27 @@ public class SearchResultsTable {
         }
     }
 
-    private static String getMatchColumnIndex(String matchType) {
+    /**
+     * Defines which column in a results table contains the values used for matching
+     * This method will need to be updated for each results table required for testing
+     */
+    private static String getMatchColumnIndex(String tableId, String matchType) {
         String matchColumnIndex = "-1";
-        if (matchType.equalsIgnoreCase("crn")) matchColumnIndex = "1";
-        if (matchType.equalsIgnoreCase("test")) matchColumnIndex = "7";
+        if (tableId.equals(NationalSearchPage.getSearchResultsTableId())
+                && matchType.equalsIgnoreCase("crn")) matchColumnIndex = "1";
+        if (tableId.contains("testTable")) {
+            if (matchType.equalsIgnoreCase("test")) matchColumnIndex = "7";
+            if (matchType.equalsIgnoreCase("crn")) matchColumnIndex = "1";
+        }
         return matchColumnIndex;
     }
+    /**
+     * Defines which column in a results table contains the View link
+     * This method will need to be updated for each results table required for testing
+     */
     private static String getViewColumnIndex(String tableId) {
         String viewLinkColumnIndex = "-1";
-        if (tableId.equalsIgnoreCase(NationalSearchPage.getSearchResultsTableId())) viewLinkColumnIndex = "11";
+        if (tableId.equals(NationalSearchPage.getSearchResultsTableId())) viewLinkColumnIndex = "11";
         if (tableId.contains("testTable")) viewLinkColumnIndex = "9";
         return viewLinkColumnIndex;
     }
