069f379b21540e8b9f0660279c221681bba3b9e7
aurinelle <aurinelle.bouekassa-taboudi@digital.justice.gov.uk>
2023-02-22 15:42:05 +0000

    added dataProvider

--- src/test/java/frameworkTests/DateUtilsTests.java
+++ src/test/java/frameworkTests/DateUtilsTests.java
@@ -1,41 +1,37 @@
 package frameworkTests;
 
 import org.testng.Assert;
+import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 
 import static utils.DateUtil.convertApiDate;
-import static utils.DateUtil.today;
+import static utils.DateUtil.getTodayDateFormatted;
 
 public class DateUtilsTests {
 
-
-    @Test
-    public void testTodayDateCorrectFormat(){
-        String today = LocalDateTime.now().format(DateTimeFormatter.ofPattern("dd/MM/yyyy"));
-        Assert.assertTrue(today().equals(today));
+    @DataProvider(name = "formatted-dates")
+    public Object[][] dpMethod() {
+        return new Object[][]{{"2000-11-17T00:00:00Z[UTC]"}, {"2000-11-17T10:15:30Z"}, {"2000-11-17T10:15:30+01:00[Europe/Paris]"}};
     }
 
     @Test
-    public void testTodayDateIncorrectFormat(){
+    public void testTodayDateCorrectFormat() {
+        String todayDate = LocalDateTime.now().format(DateTimeFormatter.ofPattern("dd/MM/yyyy"));
+        Assert.assertTrue(getTodayDateFormatted().equals(todayDate));
+    }
+
+    @Test
+    public void testTodayDateIncorrectFormat() {
         String today = LocalDateTime.now().format(DateTimeFormatter.ofPattern("dd-MM-yyyy"));
-        Assert.assertFalse(today().equals(today));
+        Assert.assertFalse(getTodayDateFormatted().equals(today));
     }
 
-
-    @Test
-    public void testConvertApiDateCorrectFormat() {
-        String apiDate = "2022-12-22T10:23:07.712Z[UTC]";
-        Assert.assertTrue(convertApiDate(apiDate).equals("22/12/2022"));
+    @Test(dataProvider = "formatted-dates")
+    public void testConvertApiDate(String date) {
+        Assert.assertEquals(convertApiDate(date), "17/11/2000");
     }
-
-    @Test
-    public void testConvertApiDateIncorrectFormat() {
-        String apiDate = "2022-12-22T10:23:07.712Z[UTC]";
-        Assert.assertFalse(convertApiDate(apiDate).equals("22-12-2022"));
-    }
-
 
 }
