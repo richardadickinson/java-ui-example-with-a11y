9dc9a3d7fbd168e16a2de1a87082b5b652b7f7a6
aurinelle <aurinelle.bouekassa-taboudi@digital.justice.gov.uk>
2023-02-23 10:00:01 +0000

    update tests

--- src/test/java/frameworkTests/DateUtilsTests.java
+++ src/test/java/frameworkTests/DateUtilsTests.java
@@ -4,34 +4,62 @@ import org.testng.Assert;
 import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 
-import java.time.LocalDateTime;
+import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
+import java.time.format.DateTimeParseException;
 
 import static utils.DateUtil.convertApiDate;
 import static utils.DateUtil.getTodayDateFormatted;
 
 public class DateUtilsTests {
 
-    @DataProvider(name = "formatted-dates")
+    @DataProvider(name = "dates")
     public Object[][] dpMethod() {
-        return new Object[][]{{"2000-11-17T00:00:00Z[UTC]"}, {"2000-11-17T10:15:30Z"}, {"2000-11-17T10:15:30+01:00[Europe/Paris]"}};
+        return new Object[][]{{"2000-11-17T00:00:00Z[UTC]"}, {"2020-01-05T10:15:30Z"}, {"1999-09-30T10:15:30+01:00[Europe/Paris]"}};
+    }
+
+    public boolean hasCorrectFormat(String dateStr, String dfp) {
+        try {
+            DateTimeFormatter.ofPattern(dfp).parse(dateStr);
+        } catch (DateTimeParseException e) {
+            return false;
+        }
+        return true;
     }
 
     @Test
-    public void testTodayDateCorrectFormat() {
-        String todayDate = LocalDateTime.now().format(DateTimeFormatter.ofPattern("dd/MM/yyyy"));
-        Assert.assertEquals(getTodayDateFormatted(), todayDate);
+    public void testTodayDateIsReturnedWithCorrectFormat() {
+        Assert.assertEquals(hasCorrectFormat(getTodayDateFormatted(), "dd/MM/yyyy"), true);
     }
 
     @Test
-    public void testTodayDateIncorrectFormat() {
-        String todayDate = LocalDateTime.now().format(DateTimeFormatter.ofPattern("dd-MM-yyyy"));
-        Assert.assertNotEquals(getTodayDateFormatted(), todayDate);
+    public void testTodayDateDoesNotReturnIncorrectFormat() {
+        Assert.assertEquals(hasCorrectFormat(getTodayDateFormatted(), "dd-MM-yyyy"), false);
     }
 
-    @Test(dataProvider = "formatted-dates")
-    public void testConvertApiDate(String date) {
-        Assert.assertEquals(convertApiDate(date), "17/11/2000");
+    @Test
+    public void testTodayDateThrowsException() {
+        Assert.assertThrows(DateTimeParseException.class, () -> {
+            LocalDate.parse(getTodayDateFormatted(), DateTimeFormatter.ofPattern("yyyy-MM-dd"));
+        });
     }
+
+    @Test(dataProvider = "dates")
+    public void testConvertedApiDateIsReturnedWithCorrectFormat(String date) {
+        Assert.assertEquals(hasCorrectFormat(convertApiDate(date), "dd/MM/yyyy"), true);
+    }
+
+    @Test(dataProvider = "dates")
+    public void testConvertedApiDateDoesNotReturnIncorrectFormat(String date) {
+        Assert.assertEquals(hasCorrectFormat(convertApiDate(date), "dd-MM-yyyy"), false);
+    }
+
+    @Test
+    public void testConvertedApiDateThrowsException() {
+        Assert.assertThrows(DateTimeParseException.class, () -> {
+            convertApiDate("2000/11/17T00:00:00Z[UTC]");
+        });
+    }
+
 
 }
