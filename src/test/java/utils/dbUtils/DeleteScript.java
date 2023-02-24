a0cbd3d6aef667f17e4f550274f33d4490c6b5e1
aurinelle <aurinelle.bouekassa-taboudi@digital.justice.gov.uk>
2023-02-24 17:03:45 +0000

    updated check method

--- src/test/java/utils/dbUtils/DeleteScript.java
+++ src/test/java/utils/dbUtils/DeleteScript.java
@@ -14,7 +14,7 @@ public class DeleteScript {
     private static final Logger logger = LoggerFactory.getLogger(DeleteScript.class);
 
     public static void deleteOffendersByPerson(ArrayList<Person> persons) {
-        for (int i = 0; i <= persons.size()-1; i++) {
+        for (int i = 0; i <= persons.size() - 1; i++) {
             deleteOffenderByCRN(persons.get(i).getCrn());
         }
     }
@@ -24,16 +24,20 @@ public class DeleteScript {
         PreparedStatement statement = assignStringValueToSqlParam(filename + ".sql", 1, crn);
         logger.info("running " + filename + " script to delete " + crn + "...");
         executeSql(statement);
-        checkIfCrnDeleted(crn);
-    }
-
-    private static void checkIfCrnDeleted(String crn)  {
-        PreparedStatement statement = assignStringValueToSqlParam("count_crn.sql", 1, crn);
-        int dbValue = Integer.parseInt(executeSqlAndReturnValue(statement));
-        if (dbValue == 0) {
+        if (crnExists(crn) == false) {
             logger.info(crn + " successfully deleted");
         } else {
             logger.error(crn + " not deleted");
+        }
+    }
+
+    public static boolean crnExists(String crn) {
+        PreparedStatement statement = assignStringValueToSqlParam("count_crn.sql", 1, crn);
+        int dbValue = Integer.parseInt(executeSqlAndReturnValue(statement));
+        if (dbValue == 0) {
+            return false;
+        } else {
+            return true;
         }
     }
 }
